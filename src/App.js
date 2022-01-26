581cc1777a5a07a20c37799f1d81daa64fdbd64b
Artsiom <kabanproff@gmail.com>
2022-01-26 08:51:51 +0300

    add previous_file and start norm

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
+import React from 'react';
+import { Route, BrowserRouter as Router } from 'react-router-dom';
 import './App.css';
+import Profile from './components/Profile/Profile';
+import Header from './components/Header/Header';
+import Navbar from './components/Navbar/Navbar';
+import Footer from './components/Footer/Footer'
+import Dialogs from './components/Dialogs/Dialogs';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const App = () => {
+	return (
+		<Router>
+			<div className='app-wrapper'>
+				<Header />
+				<Navbar />
+				<div className='app-wrapper-content'>
+					<Route path='/profile' element={Profile} />
+					<Route path='/dialogs' element={Dialogs} />
+					{/* <Profile /> */}
+				</div>
+
+				<Footer />
+
+			</div>
+		// </Router>
+
+	);
 }
 
 export default App;
