4302020c018e26ce3f389aa2c99845bc06702b71
Artsiom <kabanproff@gmail.com>
2022-01-26 21:58:28 +0300

    20 lesson end -NavLink-className={prop=>prop.isActive ? style.active : notActive

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,15 +1,21 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 import s from './Navbar.module.css';
 
-console.log(s)
+
+// className={({ isActive }) => { console.log(isActive); return (isActive ? 'active' : '') }}
+function setActive(act) {
+	return act.isActive ? s.active : '';
+}
+console.log(NavLink)
 const Navbar = () => {
 	return <nav className={s.nav}>
-		<div className={`${s.item}`}><a href="/profile/">Proffile</a></div>
-		<div className={`${s.item} ${s.active}`}><a href="/dialogs/">Messages</a></div>
-		<div className={s.item}><a href="/news">News</a></div>
-		<div className={s.item}><a href="music">Music</a></div>
-		<div className={s.item}><a href="settings">Settings</a></div>
-	</nav>
+		<div className={s.item}><NavLink to="/profile/" className={act => setActive(act)}>Proffile</NavLink></div>
+		<div className={s.item}><NavLink to="/dialogs/" className={act => setActive(act)} >Messages</NavLink></div>
+		<div className={s.item}><NavLink to="/news" className={act => setActive(act)}>News</NavLink></div>
+		<div className={s.item}><NavLink to="music" className={act => setActive(act)}>Music</NavLink></div>
+		<div className={s.item}><NavLink to="settings" className={act => setActive(act)}>Settings</NavLink></div>
+	</nav >
 }
 
 export default Navbar
