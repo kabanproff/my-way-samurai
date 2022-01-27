ba5e8de2c83f87b3701310d44484797dbe1c7108
Artsiom <kabanproff@gmail.com>
2022-01-27 14:58:38 +0300

    add Icons+spriteSvg style:dialogs and all site

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -3,8 +3,31 @@ import s from './Dialogs.module.css';
 
 const Dialogs = (p) => {
 	return (
-		<div>
-			Dialogs
+		<div className={s.dialogs}>
+			<div className={s.dialogsItems}>
+				<div className={s.dialog + ' ' + s.active}>
+					Dima
+				</div>
+				<div className={s.dialog}>
+					Lesha
+				</div>
+				<div className={s.dialog}>
+					Misha
+				</div>
+				<div className={s.dialog}>
+					Lena
+				</div>
+				<div className={s.dialog}>
+					Kiril
+				</div>
+
+			</div>
+			<div className={s.messages}>
+				<div className={s.message}>Hi</div>
+				<div className={s.message}>Hello</div>
+				<div className={s.message}>How are you doing?</div>
+				<div className={s.message}>Very Well</div>
+			</div>
 		</div>
 	)
 }
