1963a6faef4194a603394a3b90f175c53d35fe2d
Artsiom <kabanproff@gmail.com>
2022-03-16 19:26:52 +0300

    end56 presentational and container component вложили в connect контэйнерную, а вконтэйнерную тупую компоненту

---
+++ src/components/Users/UsersArrowFunction.jsx
@@ -0,0 +1,88 @@
+import axios from 'axios'
+import React from 'react'
+import s from './users.module.css'
+// import Icon from '../Icons/Icons'
+const defaultIcons = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTDpxJV62_axM0N-z8vfxnZeGBUIqclFn187w&usqp=CAU'
+
+const UsersArrowFunction = (props) => {
+
+	if (props.users.length === 0) {
+
+		// axios.get('https://api.github.com/users')
+		axios.get('https://social-network.samuraijs.com/api/1.0/users?')
+			.then(response => {
+
+				props.setUsers(response.data.items)
+			})
+
+
+		// props.setUsers(
+
+
+		// 	[
+		// 		{ id: 1, photoUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSPD2lxiEQJm9M8tY-JwUiqDkqZ45bYAYk2_Q&usqp=CAU', followed: false, fullName: 'Dmitry K', status: 'Люблю этот Мир', location: { city: 'Minsk', country: 'Belarus' } },
+		// 		{ id: 2, photoUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRp7mymjcNh2nyn-20r6qNkrMxhQUK0ogon3g&usqp=CAU', followed: true, fullName: 'Andry Sh', status: 'Хочу кушать', location: { city: 'Minsk', country: 'Belarus' } },
+		// 		{ id: 3, photoUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTB_lt9056_KbjqZQSTHvioprDk6y2eTXiTUg&usqp=CAU', followed: false, fullName: 'Ирина', status: 'Замужем', location: { city: 'Гродно', country: 'Belarus' } },
+		// 		{ id: 4, photoUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTDpxJV62_axM0N-z8vfxnZeGBUIqclFn187w&usqp=CAU', followed: true, fullName: 'Kiril', status: 'I\'am the BOSS', location: { city: 'Moscow', country: 'Rassia' } },
+		// 	]
+
+		// )
+
+	}
+	console.log(props)
+	// if (props.images.length === 0) {
+	// 	let response = async () => await fetch('https://repetitora.net/api/JS/Images')
+	// 	response().then(rs => rs.json())
+	// 		.then(rs => {
+	// 			console.log(rs)
+	// 			props.setImages(rs)
+	// 		})
+
+	// }
+	return (
+		<div className={s.wrapperUsers}>
+			<div className={s.users}>{
+				props.users.map(i => <div className={s.user} key={i.id}>
+
+					<div>
+						<div>
+							{/* <Icon name={'icon-user-male'} /> */}
+							<img alt='d' className={s.photo} src={i.photos.small !== null ? i.photos.small : defaultIcons} />
+						</div>
+						<div>
+							{i.followed
+								? <button onClick={() => { props.unfollow(i.id) }}>Follow</button>
+								: <button onClick={() => { props.follow(i.id) }}>Unfollow</button>}
+
+						</div>
+					</div>
+					<div className={s.text}>
+						<div>
+							<div>
+								{i.name}
+							</div>
+							<div>{i.status}</div>
+						</div>
+						<div>
+							<div>
+								i.location.city
+							</div>
+							<div>
+								i.location.country
+							</div>
+						</div>
+					</div>
+
+				</div>)
+			}</div>
+			<div>
+				{
+
+					props.images.map((i, d) => <img key={d} src={i.thumbnail} alt="" />)
+				}
+			</div>
+		</div>
+	)
+}
+
+export default UsersArrowFunction
--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -1,6 +1,47 @@
-import UsersC from './UsersC'
+import React from 'react'
 import { connect } from 'react-redux'
 import { followAC, setCurrentPageAC, setImagesAC, setTotalUsersCountAC, setUsersAC, unFollowAC } from '../../redux/users-reduser'
+import axios from 'axios'
+import Users from './Users'
+
+class UsersContainer extends React.Component {
+	constructor(props) {
+		super(props)
+		alert('render')
+
+	}
+
+	componentDidMount() {
+		axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`)
+			.then(response => {
+
+				this.props.setUsers(response.data.items)
+				this.props.setTotalUsersCount(response.data.totalCount)
+			})
+	}
+
+	onPageChanged = (pageNumber) => {
+		this.props.setCurrentPage(pageNumber)
+
+		axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`)
+			.then(response => {
+
+				this.props.setUsers(response.data.items)
+			})
+
+	}
+
+	render() {
+		console.log(this.props)
+		return <Users pageSize={this.props.pageSize}
+			onPageChanged={this.onPageChanged}
+			currentPage={this.props.currentPage}
+			users={this.props.users}
+			unfollow={this.props.unfollow}
+			follow={this.props.follow}
+		/>
+	}
+}
 
 const mapStateToProps = (state) => {
 	return {
@@ -35,4 +76,4 @@ const mapDispatchToProps = (dispatch) => {
 	}
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(UsersC)
+export default connect(mapStateToProps, mapDispatchToProps)(UsersContainer)
